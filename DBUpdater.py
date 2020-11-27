fcee599fdb0e20b1204d8bc8f3aae8eb2f4a3b20
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2020-11-27 20:58:57 +0900

    update

--- DBUpdater.py
+++ DBUpdater.py
@@ -49,12 +49,22 @@ class DBUpdater:
         """
         url = 'https://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
         krx = pd.read_html(url, header=0)[0]
+        krx = krx[['종목코드', '회사명']]
+        krx = krx.rename(columns={'종목코드':'code','회사명':'company'})
+        krx.code = krx.code.map('{:06d}'.format)
     
     def update_comp_info(self):
         """
         update code in companyInfo table
         save dict
+        with replace into
         """
+        sql = "select * from companyInfo"
+        df = pd.read_sql(sql, self.conn)
+        for idx in range(len(df)):
+            self.codes[df['code'].values[idx]]=df['company'].values[idx]
+        with self.conn.cursor() as curs:
+            sql = "select max(last_update) from company_info"
 
     def read_naver(self, code, company, pages_to_fetch):
         """
