cc4ea4337f21e244af029b488217a4f3f0f16978
Ui Hyeon Lee <ksw09157.5212@gmail.com>
2020-12-06 18:33:25 +0900

    fix bug

--- DBUpdater.py
+++ DBUpdater.py
@@ -78,7 +78,7 @@ class DBUpdater:
                 for idx in range(len(krx)):
                     code = krx.code.values[idx]
                     company = krx.company.values[idx]
-                    sql =  f"replace into companyInfo (code, company, lastUpdate) values ('{code}','{company}','{today}'"
+                    sql =  f"replace into companyInfo (code, company, lastUpdate) values ('{code}','{company}','{today}')"
                     curs.execute(sql) # replace into 구문을 통해 종목코드, 회사명, 오늘 날짜' 행을 DB에 저장한다.
                     self.codes[code] = company
                     tmnow = datetime.now().strftime('%Y-%m-%d %H:%M')
